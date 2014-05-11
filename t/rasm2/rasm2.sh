e1a14ae4dd75c9e6efb93df761c736a826a8615c
Maijin <maijin@malware.lu>
2014-05-11 14:04:27 +0200

    fix rasm2 test

--- t/rasm2/rasm2.sh
+++ t/rasm2/rasm2.sh
@@ -1,43 +1,19 @@
 #!/bin/sh
+for a in . .. ../.. ; do [ -e $a/tests.sh ] && . $a/tests.sh ; done
 
-SRC="
-\nmain:
-\n        push rbp
-\n        mov rbp, rsp
-\n        sub rsp, 0x20
-\n        cmp eax, 0
-\n        jnz fail
-\nfine:
-\n        mov eax, 0
-\n        jmp end
-\nfail:
-\n        mov eax, 1
-\nend:
-\n        add rsp, 0x20
-\n        pop rbp
-\n        ret
-"
-BIN=5589e583ec2083f8000f8507000000b800000000eb05b80100000083c4205dc3
 
-rasm() {
-	rasm2 -f $1
-}
-
-runtest() {
-	printf "     $1\t$2"
-	O=`eval $2 "$1"`
-	if [ "$O" = "$3" ]; then
-		echo "\r OK"
-		TESTS_SUCCESS=$((${TESTS_SUCCESS}+1))
-		return 0
-	else
-		TESTS_FAILED=$((${TESTS_FAILED}+1))
-		echo "  *** FAIL because '$3' not '$O' ***"
-		return 1
-	fi
-}
-
-echo $SRC > test.rasm
-runtest test.rasm rasm $BIN
-rm -f test.rasm
-TESTS_TOTAL=$((${TESTS_TOTAL}+1))
+NAME='Rasm2'
+CMDS="!rasm2 -a x86 -b 64 -d '5589e583ec2083f8000f8507000000b800000000eb05b80100000083c4205dc3' "
+EXPECT='push rbp
+mov ebp, esp
+sub esp, 0x20
+cmp eax, 0x0
+jne 0x16
+mov eax, 0x0
+jmp 0x1b
+mov eax, 0x1
+add esp, 0x20
+pop rbp
+ret
+'
+run_test

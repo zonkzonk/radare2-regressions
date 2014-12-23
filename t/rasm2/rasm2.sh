cca20c85025ca5ef1da64e052072a3378c1189e7
Andrew McDonnell <bugs@andrewmcdonnell.net>
2014-12-23 23:21:14 +1030

    Added unit test for github issue #1190

--- t/rasm2/rasm2.sh
+++ t/rasm2/rasm2.sh
@@ -29,3 +29,36 @@ CMDS="!rasm2 -a x86 -b 64 'test rax, rbx' "
 EXPECT='4885d8
 '
 run_test
+
+NAME='Rasm2 #1190'
+CMDS="!rasm2 -a x86 -b 32 'mov [0x33], eax'
+!rasm2 -a x86 -b 32 'mov eax, 33'
+!rasm2 -a x86 -b 32 'mov eax, 0x33'
+"
+EXPECT='890533000000
+b821000000
+b833000000
+'
+run_test
+
+NAME='Rasm2 #1190 (detect syntax error - case 1)'
+CMDS="!rasm2 -a x86 -b 32 'mov hello, eax'"
+EXPECT_ERR="Cannot assemble 'mov hello, eax' at line 3
+invalid
+"
+run_test
+
+NAME='Rasm2 #1190 (detect syntax error - case 2)'
+CMDS="!rasm2 -a x86 -b 32 'mov 33, eax'"
+EXPECT_ERR="Cannot assemble 'mov hello, eax' at line 3
+invalid
+"
+run_test
+
+NAME='Rasm2 #1190 (detect syntax error - case 3)'
+CMDS="!rasm2 -a x86 -b 32 'mov eax, hello'"
+BROKEN=1
+EXPECT_ERR="Cannot assemble 'mov hello, eax' at line 3
+invalid
+"
+run_test

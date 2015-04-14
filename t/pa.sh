b745e2f6e6341925f79908d260c49b28e8ec9957
Maijin <maijin@malware.lu>
2015-04-14 20:54:07 +0200

    Rename testfile and move pa test to t

---
+++ t/pa.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+for a in . .. ../.. ; do [ -e $a/tests.sh ] && . $a/tests.sh ; done
+
+
+NAME='r2 asm with pa'
+CMDS="e asm.arch=x86
+e asm.bits=32
+f TEST=33
+pa mov eax, TEST
+"
+BROKEN=1
+EXPECT="b821000000
+"
+run_test
+
+NAME='r2 asm with pa'
+CMDS="e asm.arch=x86
+e asm.bits=32
+f TEST=33
+pa mov eax, 33
+"
+EXPECT="b821000000
+"
+run_test

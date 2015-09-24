6958ad9a43de8f1bffffd21694cbdef7b2b48654
Jeffrey Crowell <crowell@bu.edu>
2015-09-24 00:43:19 -0400

    indent with tabs for unit tests

--- unit/test_base64.c
+++ unit/test_base64.c
@@ -5,35 +5,35 @@ int tests_run = 0;
 char* buf;
 
 char* test_r_base64_decode(void) {
-  char* hello = r_base64_decode_dyn ("aGVsbG8=", -1);
-  snprintf(buf, 1024, "error, b64decode(hello) failed : %s", hello);
-  mu_assert(buf, strcmp(hello, "hello") == 0);
-  return NULL;
+	char* hello = r_base64_decode_dyn ("aGVsbG8=", -1);
+	snprintf(buf, 1024, "error, b64decode(hello) failed : %s", hello);
+	mu_assert(buf, strcmp(hello, "hello") == 0);
+	return NULL;
 }
 
 char* test_r_base64_encode(void) {
-  char* hello = r_base64_encode_dyn("hello", -1);
-  snprintf(buf, 1024, "error, b64encode(hello) != %s", hello);
-  mu_assert(buf, strcmp(hello, "aGVsbG8=") == 0);
-  return NULL;
+	char* hello = r_base64_encode_dyn("hello", -1);
+	snprintf(buf, 1024, "error, b64encode(hello) != %s", hello);
+	mu_assert(buf, strcmp(hello, "aGVsbG8=") == 0);
+	return NULL;
 }
 
 char* all_tests() {
-  mu_run_test(test_r_base64_decode);
-  mu_run_test(test_r_base64_encode);
-  return NULL;
+	mu_run_test(test_r_base64_decode);
+	mu_run_test(test_r_base64_encode);
+	return NULL;
 }
 
 int main(int argc, char **argv) {
-  buf = malloc(1024);
-  char *result = all_tests();
-  if (result != 0) {
-    printf("%s\n", result);
-  }
-  else {
-    printf("ALL TESTS PASSED\n");
-  }
-  printf("Tests run: %d\n", tests_run);
+	buf = malloc(1024);
+	char *result = all_tests();
+	if (result != 0) {
+		printf("%s\n", result);
+	}
+	else {
+		printf("ALL TESTS PASSED\n");
+	}
+	printf("Tests run: %d\n", tests_run);
 
-  return result != 0;
+	return result != 0;
 }
--- unit/test_list.c
+++ unit/test_list.c
@@ -5,50 +5,50 @@ int tests_run = 0;
 char* buf;
 
 char* test_r_list_size(void) {
-  // Test that r_list adding and deleting works correctly.
-  int i;
-  RList* list = r_list_new ();
-  intptr_t test = 0x101010;
-  // Add 100 items.
-  for (i = 0; i < 100; ++i) {
-    r_list_append (list, (void*)test);
-    snprintf(buf, 1024, "error, append gave wrong length. actual %d, expected %d",
-        r_list_length (list), i + 1);
-    mu_assert(buf, i + 1 == r_list_length (list));
-  }
-  // Delete 50 of them.
-  for (i = 0; i < 50; ++i) {
-    intptr_t val = (intptr_t)r_list_pop (list);
-    snprintf(buf, 1024, "error, pop gave wrong length. actual: %d, expected %d",
-        r_list_length (list), 99 - i);
-    mu_assert(buf, 99 - i == r_list_length (list));
-  }
-  // Purge the list.
-  r_list_purge (list);
-  mu_assert("error, purge didn't clear list", 0 == r_list_length (list));
-  return NULL;
+	// Test that r_list adding and deleting works correctly.
+	int i;
+	RList* list = r_list_new ();
+	intptr_t test = 0x101010;
+	// Add 100 items.
+	for (i = 0; i < 100; ++i) {
+		r_list_append (list, (void*)test);
+		snprintf(buf, 1024, "error, append gave wrong length. actual %d, expected %d",
+				r_list_length (list), i + 1);
+		mu_assert(buf, i + 1 == r_list_length (list));
+	}
+	// Delete 50 of them.
+	for (i = 0; i < 50; ++i) {
+		intptr_t val = (intptr_t)r_list_pop (list);
+		snprintf(buf, 1024, "error, pop gave wrong length. actual: %d, expected %d",
+				r_list_length (list), 99 - i);
+		mu_assert(buf, 99 - i == r_list_length (list));
+	}
+	// Purge the list.
+	r_list_purge (list);
+	mu_assert("error, purge didn't clear list", 0 == r_list_length (list));
+	return NULL;
 }
 
 char* test_r_list_values(void) {
-  RList* list = r_list_new ();
-  intptr_t test1 = 0x12345;
-  intptr_t test2 = 0x88888;
-  r_list_append (list, (void*)test1);
-  r_list_append (list, (void*)test2);
-  mu_assert("error, first value not 0x88888",
-      (intptr_t)0x88888 == (intptr_t)r_list_pop (list));
-  mu_assert("error, first value not 0x12345",
-      (intptr_t)0x12345 == (intptr_t)r_list_pop (list));
-  return NULL;
+	RList* list = r_list_new ();
+	intptr_t test1 = 0x12345;
+	intptr_t test2 = 0x88888;
+	r_list_append (list, (void*)test1);
+	r_list_append (list, (void*)test2);
+	mu_assert("error, first value not 0x88888",
+			(intptr_t)0x88888 == (intptr_t)r_list_pop (list));
+	mu_assert("error, first value not 0x12345",
+			(intptr_t)0x12345 == (intptr_t)r_list_pop (list));
+	return NULL;
 }
 
 char* test_r_list_join(void) {
-  RList* list1 = r_list_new ();
-  RList* list2 = r_list_new ();
-  intptr_t test1 = 0x12345;
-  intptr_t test2 = 0x88888;
-  r_list_append (list1, (void*)test1);
-  r_list_append (list2, (void*)test2);
+	RList* list1 = r_list_new ();
+	RList* list2 = r_list_new ();
+	intptr_t test1 = 0x12345;
+	intptr_t test2 = 0x88888;
+	r_list_append (list1, (void*)test1);
+	r_list_append (list2, (void*)test2);
 	mu_assert("r_list_join returned non-success",
 			r_list_join (list1, list2) == 1);
 	mu_assert("r_list_join two single element lists result length is 1",
@@ -65,59 +65,59 @@ char* test_r_list_free(void) {
 }
 
 char* test_r_list_del_n(void) {
-  RList* list = r_list_new ();
-  intptr_t test1 = 0x12345;
-  intptr_t test2 = 0x88888;
-  r_list_append (list, (void*)test1);
-  r_list_append (list, (void*)test2);
+	RList* list = r_list_new ();
+	intptr_t test1 = 0x12345;
+	intptr_t test2 = 0x88888;
+	r_list_append (list, (void*)test1);
+	r_list_append (list, (void*)test2);
 	snprintf (buf, 1024, "error, expected size 2, got %d\n", (int)r_list_length (list));
-  mu_assert(buf, r_list_length (list) == 2);
+	mu_assert(buf, r_list_length (list) == 2);
 	r_list_del_n (list, 0);
-  mu_assert("error, first value not 0x88888",
-      (intptr_t)0x88888 == (intptr_t)r_list_pop (list));
-  return NULL;
+	mu_assert("error, first value not 0x88888",
+			(intptr_t)0x88888 == (intptr_t)r_list_pop (list));
+	return NULL;
 }
 
 char* test_r_list_sort(void) {
 	int bad = 0;
-  RList* list = r_list_new ();
-  char* test1 = "AAAA";
-  char* test2 = "BBBB";
-  char* test3 = "CCCC";
+	RList* list = r_list_new ();
+	char* test1 = "AAAA";
+	char* test2 = "BBBB";
+	char* test3 = "CCCC";
 	// Put in not sorted order.
-  r_list_append (list, (void*)test1);
-  r_list_append (list, (void*)test3);
-  r_list_append (list, (void*)test2);
+	r_list_append (list, (void*)test1);
+	r_list_append (list, (void*)test3);
+	r_list_append (list, (void*)test2);
 	// Sort.
 	r_list_sort (list, (RListComparator)strcmp);
 	// Check that the list is actually sorted.
 	bad |= strcmp ("AAAA", list->head->data);
 	bad |= strcmp ("BBBB", list->head->n->data);
 	bad |= strcmp ("CCCC", list->head->n->n->data);
-  mu_assert("error, list not sorted properly", bad == 0);
-  return NULL;
+	mu_assert("error, list not sorted properly", bad == 0);
+	return NULL;
 }
 
 char* all_tests() {
-  mu_run_test(test_r_list_size);
-  mu_run_test(test_r_list_values);
-  mu_run_test(test_r_list_join);
-  mu_run_test(test_r_list_free);
-  mu_run_test(test_r_list_del_n);
-  mu_run_test(test_r_list_sort);
-  return NULL;
+	mu_run_test(test_r_list_size);
+	mu_run_test(test_r_list_values);
+	mu_run_test(test_r_list_join);
+	mu_run_test(test_r_list_free);
+	mu_run_test(test_r_list_del_n);
+	mu_run_test(test_r_list_sort);
+	return NULL;
 }
 
 int main(int argc, char **argv) {
-  buf = malloc(1024);
-  char *result = all_tests();
-  if (result != 0) {
-    printf("%s\n", result);
-  }
-  else {
-    printf("ALL TESTS PASSED\n");
-  }
-  printf("Tests run: %d\n", tests_run);
+	buf = malloc(1024);
+	char *result = all_tests();
+	if (result != 0) {
+		printf("%s\n", result);
+	}
+	else {
+		printf("ALL TESTS PASSED\n");
+	}
+	printf("Tests run: %d\n", tests_run);
 
-  return result != 0;
+	return result != 0;
 }

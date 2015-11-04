d3612e8ca47d76fd9c4f94004f04409068e733c6
pancake <pancake@nopcode.org>
2015-11-05 00:41:24 +0100

    Set R*_NOPLUGINS env var to speedup tests (30% faster)

--- tests.sh
+++ tests.sh
@@ -17,6 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+export RABIN2_NOPLUGINS=1
+export RASM2_NOPLUGINS=1
+export R2_NOPLUGINS=1
+
 GREP="$1"
 GREP=""
 SKIP=0

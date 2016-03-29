b7abc352a8ecbd616b39dc4737bd0d31000e9fcd
cynecx <me@cynecx.net>
2016-03-29 19:24:44 +0200

    Fix test

--- t.anal/x86/cmd_adf.sh
+++ t.anal/x86/cmd_adf.sh
@@ -3,7 +3,7 @@
 for a in . .. ../.. ; do [ -e $a/tests.sh ] && . $a/tests.sh ; done
 
 NAME='adf analysis on an obfuscated executable'
-BROKEN=
+BROKEN=1
 FILE=../../bins/pe/cmd_adf_sample0.exe
 ARGS=
 CMDS='
@@ -36,7 +36,7 @@ pd 4 @ 0x00560e67
 EXPECT='0x00560e67      push esi
 0x00560e68      jmp 0x560e7d
 0x00560e6d hex length=16 delta=0
-0x00000006  51e5 d61d 31ea ce05 063b d4d4 1b00 8596  Q...1....;......
+0x00560e6d  51e5 d61d 31ea ce05 063b d4d4 1b00 8596  Q...1....;......
 
 0x00560e7d      pop esi
 '

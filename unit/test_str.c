b2da21e1c4305fd6432d6f7e3fee7f42f8ca4abd
Jeffrey Crowell <crowell@bu.edu>
2016-06-09 00:22:13 -0400

    add a test for r_str_bits64

--- unit/test_str.c
+++ unit/test_str.c
@@ -17,9 +17,21 @@ bool test_r_str_replace_char(void) {
 	mu_end;
 }
 
+bool test_r_str_bits64(void) {
+	char buf[65];
+	(void)r_str_bits64 (buf, 0);
+	mu_assert_streq (buf, "00000000", "binary of 0");
+	(void)r_str_bits64 (buf, 1);
+	mu_assert_streq (buf, "00000001", "binary of 1");
+	(void)r_str_bits64 (buf, 2);
+	mu_assert_streq (buf, "00000010", "binary of 2");
+	mu_end;
+}
+
 bool all_tests() {
 	mu_run_test(test_r_str_replace_char_once);
 	mu_run_test(test_r_str_replace_char);
+	mu_run_test(test_r_str_bits64);
 	return tests_passed != tests_run;
 }
 

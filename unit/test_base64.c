7ef5d7fd3ffe1ebc22d3b2e26184a212fd1dbd3c
Jeffrey Crowell <crowell@bu.edu>
2016-06-18 13:07:59 -0400

    test non-dyn base64 encoding as that is a different codepath

--- unit/test_base64.c
+++ unit/test_base64.c
@@ -8,8 +8,16 @@ bool test_r_base64_decode(void) {
 	mu_end;
 }
 
-int test_r_base64_encode(void) {
+int test_r_base64_encode_dyn(void) {
 	char* hello = r_base64_encode_dyn("hello", -1);
+	mu_assert_streq(hello, "aGVsbG8=", "base64_encode_dyn");
+	free (hello);
+	mu_end;
+}
+
+int test_r_base64_encode(void) {
+	char* hello = malloc (50);
+	r_base64_encode(hello, "hello", -1);
 	mu_assert_streq(hello, "aGVsbG8=", "base64_encode_dyn");
 	free (hello);
 	mu_end;
@@ -17,6 +25,7 @@ int test_r_base64_encode(void) {
 
 int all_tests() {
 	mu_run_test(test_r_base64_decode);
+	mu_run_test(test_r_base64_encode_dyn);
 	mu_run_test(test_r_base64_encode);
 	return tests_passed != tests_run;
 }

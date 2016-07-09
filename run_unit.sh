66449349e1fe7271891093149af2ec8c86bf1c6b
Jeffrey Crowell <crowell@bu.edu>
2016-07-09 01:56:42 -0400

    fix run_unit.sh to work on my machine

--- run_unit.sh
+++ run_unit.sh
@@ -4,7 +4,7 @@
 # export KCOV="kcov /path/to/output"
 # kcov output will be placed in the /path/to/output/index.html
 
-for i in $(find ./unit -name 'test_*' -type f -perm +111); do
+for i in $(find ./unit -name 'test_*' -type f -perm /111); do
 	filename=$(basename "$i")
 	echo "$filename"
 	${KCOV} $i

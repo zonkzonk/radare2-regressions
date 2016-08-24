362c7c80b4cd140b743e2e38fe621ce37c528e49
Rakholiya Jenish <P4N74@users.noreply.github.com>
2016-08-24 20:04:39 +0530

    add test (#518)

--- unit/test_str.c
+++ unit/test_str.c
@@ -172,6 +172,25 @@ bool test_r_sub_str_rchr(void) {
 	mu_end;
 }
 
+bool test_r_str_rchr(void) {
+	const char* test = "raddddare2d";
+	const char* out = r_str_rchr (test, NULL, '2');
+	mu_assert_streq (out, "2d", "pointer to last p in range in raddddare2d");
+	out = r_str_rchr (test, NULL, 'p');
+	if (out) {
+		mu_assert ("non NULL value returned", 0);
+	}
+	out = test + 9;
+	out = r_str_rchr (test, out, 'd');
+	mu_assert_streq (out, "dare2d", "pointer to last d in range in raddddare2d");
+	out = test + strlen (test);
+	out = r_str_rchr (test, out, 'p');
+	if (out) {
+		mu_assert ("non NULL value of out", 0);
+	}
+	mu_end;
+}
+
 bool all_tests() {
 	mu_run_test(test_r_str_replace_char_once);
 	mu_run_test(test_r_str_replace_char);
@@ -188,6 +207,7 @@ bool all_tests() {
 	mu_run_test(test_r_str_lchr);
 	mu_run_test(test_r_sub_str_lchr);
 	mu_run_test(test_r_sub_str_rchr);
+	mu_run_test(test_r_str_rchr);
 	return tests_passed != tests_run;
 }
 
